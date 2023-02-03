399e3c2fd3efab594fe5ef7ac43ea320d1757bd7
hk-bui <bui.h.kien@gmail.com>
2023-02-03 17:16:06 +0100

    Update copyright.m

--- src/copyright.m
+++ src/copyright.m
@@ -1,5 +1,11 @@
 function cr = copyright()
 
+%# **Champ3d Project**
+%_Code for Computational Physics_
+%Author : Huu-Kien Bui, IREENA Lab - UR 4642, Nantes Universite'
+%Huu-Kien.Bui@univ-nantes.fr
+%Copyright (c) 2023 H-K. Bui, All Rights Reserved.
+
 cr = ['Champ3d Project - Copyright (c) 2022 Huu-Kien Bui, ' ...
 'IREENA Lab - UR 4642, Nantes Universite, ' ...
 'Huu-Kien.Bui@univ-nantes.fr'];
