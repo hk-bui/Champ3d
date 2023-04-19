bfa0444c38b013b0770ca2e6f5dbc9ffb98b9c5d
hk-bui <bui.h.kien@gmail.com>
2023-04-20 00:31:58 +0200

    fix nomesh

--- src/interface/f_add_nomesh.m
+++ src/interface/f_add_nomesh.m
@@ -56,7 +56,39 @@ else
     design3d.nomesh(iec+1).id_elem  = id_elem;
 end
 
+%--------------------------------------------------------------------------
+con = f_connexion(design3d.mesh.elem_type);
+nbNo_inFa_max = max(con.nbNo_inFa);
+%---------
+bcmesh = f_make_mds(design3d.mesh.node,...
+                    design3d.mesh.elem(:,design3d.nomesh(iec+1).id_elem),...
+                    design3d.mesh.elem_type);
+%--------------------------------------------------------------------------
+id_face = ...
+    f_findvec(bcmesh.bound(1:nbNo_inFa_max,:),design3d.mesh.face(1:nbNo_inFa_max,:));
+design3d.nomesh(iec+1).id_face = id_face;
+design3d.nomesh(iec+1).s_face = f_measure(design3d.mesh.node,design3d.mesh.face(:,id_face),'face');
+%--------------------------------------------------------------------------
+nbEd_inFa_max = 0;
+for i = 1:length(con.nbEd_inFa)
+    nbEd_inFa_max = max([nbEd_inFa_max con.nbEd_inFa{i}]);
 end
-
-
+%--------------------------------------------------------------------------
+id_edge = reshape(design3d.mesh.edge_in_face(1:nbEd_inFa_max,id_face),...
+                  1,nbEd_inFa_max*length(id_face));
+id_edge = unique(id_edge);
+id_edge(id_edge == 0) = [];
+design3d.nomesh(iec+1).id_edge = id_edge;
+%--------------------------------------------------------------------------
+id_node = reshape(design3d.mesh.edge(1:2,id_edge),...
+                  1,2*length(id_edge));
+id_node = unique(id_node);
+id_node(id_node == 0) = [];
+design3d.nomesh(iec+1).id_node = id_node;
+%--------------------------------------------------------------------------
+all_edge = design3d.mesh.edge_in_elem(1:con.nbEd_inEl,design3d.nomesh(iec+1).id_elem);
+id_inside_edge = setdiff(all_edge, id_edge);
+id_inside_edge = unique(id_inside_edge);
+id_inside_edge(id_inside_edge == 0) = [];
+design3d.nomesh(iec+1).id_inside_edge = id_inside_edge;
 
