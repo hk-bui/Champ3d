e15da92cc121a4662cda017fc4170a770f7580ae
hk-bui <bui.h.kien@gmail.com>
2023-10-11 21:24:17 +0200

    minor changes

--- src/coretools/f_foreach.m
+++ src/coretools/f_foreach.m
@@ -7,10 +7,11 @@ function vout = f_foreach(f,varargin)
 %--------------------------------------------------------------------------
 
 % --- valid argument list (to be updated each time modifying function)
-arglist = {'argument_array'};
+arglist = {'argument_array','nb_elem'};
 
 % --- default input value
 argument_array = {};
+nb_elem = [];
 
 % --- check and update input
 for i = 1:length(varargin)/2
@@ -26,24 +27,77 @@ if ~isa(f,'function_handle')
 end
 %--------------------------------------------------------------------------
 argument_array = f_to_scellargin(argument_array);
-if f_nargin(f) ~= length(argument_array)
+nb_arg = length(argument_array);
+nb_fargin = f_nargin(f);
+%--------------------------------------------------------------------------
+if nb_fargin ~= length(argument_array)
     error([mfilename ': Check number of f arguments !']);
 end
 %--------------------------------------------------------------------------
-nb_arg = length(argument_array);
 nb_elem__ = zeros(1,nb_arg);
+size_arg = {};
+len_size = {};
 for i = 1:nb_arg
-    nb_elem__(i) = length(argument_array{i});
+    size_arg{i}  = size(argument_array{i});
+    len_size{i}  = length(size_arg{i});
+    nb_elem__(i) = max(size_arg{i});
 end
 %--------------------------------------------------------------------------
-if nb_arg > 1
-    if ~all(diff(nb_elem__) == 0)
-        error([mfilename ': Check size of #argument_array !']);
+if isempty(nb_elem)
+    nb_elem = max(nb_elem__);
+end
+%--------------------------------------------------------------------------
+poidelem = zeros(1,nb_arg);
+for i = 1:nb_arg
+    po = find(size_arg{i} == nb_elem);
+    if ~isempty(po)
+        poidelem(i) = po;
     end
 end
 %--------------------------------------------------------------------------
-nb_elem = nb_elem__(1);
-vout = 
+% if nb_arg > 1
+%     if ~all(diff(nb_elem__) == 0)
+%         error([mfilename ': Check size of #argument_array !']);
+%     end
+% end
+%--------------------------------------------------------------------------
+% Test (brute)
+
+a = {};
+for i = 1:nb_fargin
+    argu = argument_array{i};
+    if poidelem(i)
+        
+    end
+end
+
+
+if nb_fargin == 0
+    vtest = feval(f);
+elseif nb_fargin == 1
+    
+    a1 = argument_array{1};
+    vtest = feval(f,argument_array{1});
+elseif nb_fargin == 2
+    vtest = feval(f,argument_array{1},argument_array{2});
+elseif nb_fargin == 3
+    vtest = feval(f,argument_array{1},argument_array{2},argument_array{3});
+elseif nb_fargin == 4
+    vtest = feval(f,argument_array{1},argument_array{2},argument_array{3},argument_array{4});
+elseif nb_fargin == 5
+    vtest = feval(f,argument_array{1},argument_array{2},argument_array{3},argument_array{4},argument_array{5});
+elseif nb_fargin == 6
+    vtest = feval(f,argument_array{1},argument_array{2},argument_array{3},argument_array{4},argument_array{5},argument_array{6});
+end
+%--------------------------------------------------------------------------
+sizev = size(vtest);
+vout = zeros([nb_elem sizev]);
+for i = 1:nb_elem
+    vout()
+end
+
+
+
 
 
 
