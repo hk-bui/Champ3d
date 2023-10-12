210b08cafafadaa71384b2412987ae9c27e20ce9
hk-bui <62320126+hk-bui@users.noreply.github.com>
2023-10-12 18:24:34 +0200

    Update f_foreach.m

--- src/coretools/f_foreach.m
+++ src/coretools/f_foreach.m
@@ -6,6 +6,7 @@ function vout = f_foreach(f,varargin)
 % Copyright (c) 2022 H-K. Bui, All Rights Reserved.
 %--------------------------------------------------------------------------
 
+
 % --- valid argument list (to be updated each time modifying function)
 arglist = {'argument_array','nb_elem'};
 
@@ -55,56 +56,89 @@ for i = 1:nb_arg
     end
 end
 %--------------------------------------------------------------------------
+arg_pattern = {};
+for i = 1:nb_arg
+    ap = '(';
+    for j = 1:len_size{i}
+        if j == poidelem(i)
+            ap = [ap 'id_elem,'];
+        else
+            ap = [ap ':,'];
+        end
+    end
+    ap(end) = [];
+    ap = [ap ')'];
+    arg_pattern{i} = ap;
+end
+%--------------------------------------------------------------------------
 % if nb_arg > 1
 %     if ~all(diff(nb_elem__) == 0)
 %         error([mfilename ': Check size of #argument_array !']);
 %     end
 % end
 %--------------------------------------------------------------------------
-% Test (brute)
-
+% Test
 a = {};
 for i = 1:nb_fargin
-    argu = argument_array{i};
-    if poidelem(i)
-        
-    end
+    id_elem = 1;
+    eval(['a{i} = argument_array{i}' arg_pattern{i}]);
 end
-
-
+%--------------------------------------------------------------------------
 if nb_fargin == 0
-    vtest = feval(f);
+    vtest = f();
 elseif nb_fargin == 1
-    
-    a1 = argument_array{1};
-    vtest = feval(f,argument_array{1});
+    vtest = f(a{1});
 elseif nb_fargin == 2
-    vtest = feval(f,argument_array{1},argument_array{2});
+    vtest = f(a{1},a{2});
 elseif nb_fargin == 3
-    vtest = feval(f,argument_array{1},argument_array{2},argument_array{3});
+    vtest = f(a{1},a{2},a{3});
 elseif nb_fargin == 4
-    vtest = feval(f,argument_array{1},argument_array{2},argument_array{3},argument_array{4});
+    vtest = f(a{1},a{2},a{3},a{4});
 elseif nb_fargin == 5
-    vtest = feval(f,argument_array{1},argument_array{2},argument_array{3},argument_array{4},argument_array{5});
+    vtest = f(a{1},a{2},a{3},a{4},a{5});
 elseif nb_fargin == 6
-    vtest = feval(f,argument_array{1},argument_array{2},argument_array{3},argument_array{4},argument_array{5},argument_array{6});
+    vtest = f(a{1},a{2},a{3},a{4},a{5},a{6});
 end
 %--------------------------------------------------------------------------
 sizev = size(vtest);
-vout = zeros([nb_elem sizev]);
-for i = 1:nb_elem
-    vout()
+vout  = zeros([nb_elem sizev]);
+if numel(vtest) == 1
+    len_size_vout = 1;
+else
+    len_size_vout = length(sizev);
 end
-
-
-
-
-
-
-
-
-
-
-
+%--------------------------------------------------------------------------
+vout_pattern = '(id_elem,';
+for i = 1:len_size_vout
+    vout_pattern = [vout_pattern ':,'];
+end
+vout_pattern(end) = [];
+vout_pattern = [vout_pattern ')'];
+%--------------------------------------------------------------------------
+for id_elem = 1:nb_elem
+    %----------------------------------------------------------------------
+    a = {};
+    for i = 1:nb_fargin
+        eval(['a{i} = argument_array{i}' arg_pattern{i}]);
+    end
+    %----------------------------------------------------------------------
+    if nb_fargin == 0
+        eval(['vout' vout_pattern  '= f();']);
+    elseif nb_fargin == 1
+        eval(['vout' vout_pattern '= f(a{1});']);
+    elseif nb_fargin == 2
+        eval(['vout' vout_pattern '= f(a{1},a{2});']);
+    elseif nb_fargin == 3
+        eval(['vout' vout_pattern '= f(a{1},a{2},a{3});']);
+    elseif nb_fargin == 4
+        eval(['vout' vout_pattern '= f(a{1},a{2},a{3},a{4});']);
+    elseif nb_fargin == 5
+        eval(['vout' vout_pattern '= f(a{1},a{2},a{3},a{4},a{5});']);
+    elseif nb_fargin == 6
+        eval(['vout' vout_pattern '= f(a{1},a{2},a{3},a{4},a{5},a{6});']);
+    end
+end
+%--------------------------------------------------------------------------
+vout = squeeze(vout);
 
 
